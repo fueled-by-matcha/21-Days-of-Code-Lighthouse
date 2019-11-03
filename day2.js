44c3f0775cd5db409ca3597bfb140d0127ed006e
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2019-11-03 11:08:03 -0500

    Create day2.js

---
+++ day2.js
@@ -0,0 +1,16 @@
+const interviewAnswer = (topic) => {
+  // Code here!
+  if(topic == "arts funding"){
+    return "We'll have to get creative!";
+  }
+  else if(topic == "economy"){
+    return "Time is money.";
+  }
+  else if(topic == "transportation"){
+    return "It's going to be a long road, so we better get moving.";
+  }
+  else{
+    return"No comment.";
+  }
+  // Remember to return a value!
+}
