15de4b611d6748165d12163ab8fcf388e6bea20e
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2019-11-05 13:39:20 -0500

    Create day5.js

---
+++ day5.js
@@ -0,0 +1,14 @@
+const chooseStations = (stations) => {
+  // Code here!
+  let goodStations = [];
+  for (const station of stations){
+    let name = station[0];
+    let capacity = station[1];
+    let type = station[2];
+    if(capacity >=20 && (type === "school" || type === "community centre")){
+      goodStations.push(name);
+    }
+  }
+  return goodStations;
+  // Remember to return a value!
+}
