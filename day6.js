1380b7f320715bd2e13b71b162b23c9633683acf
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2019-11-06 16:26:22 -0500

    Create day6.js

---
+++ day6.js
@@ -0,0 +1,15 @@
+const voterTurnout = (voter_signatures, voter_ids) => {
+  // Code here!
+  if(voter_signatures.length !== voter_ids.length){
+    return false;
+  }
+  else{
+    for(let i = 0;i<voter_signatures.length; i++){
+      if(voter_signatures[i] !== voter_ids[i]){
+        return "FRAUD!";
+      }
+    }
+    return "All clear, we can count the votes!";
+  }
+  
+}
